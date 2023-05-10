120bc9a8b3500f5d6e94f8f7a8931f053a31f5e6
RGokulravichandran <rgokulravichandran@gmail.com>
2023-05-10 09:00:39 +0530

    vercel api update

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-export const API = "https://back-3ct5jfh8v-rgokulravichandran.vercel.app";
+export const API = "https://back-end-lovat-theta.vercel.app/";
 // export const API = "http://localhost:4000";
